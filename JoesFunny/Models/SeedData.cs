5c18ba5a4b367a7dd538ce6feb8108879f58ff7e
JoeYalden <joeyald@outlook.com>
2024-01-29 16:57:46 +0000

    Added a seed for the database

---
+++ JoesFunny/Models/SeedData.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using JoesFunny.Data;
+using System;
+using System.Linq;
+
+namespace JoesFunny.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new JoesFunnyContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<JoesFunnyContext>>()))
+        {
+            // Look for any PersonalInformations.
+            if (context.PersonalInformation.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.PersonalInformation.AddRange(
+                new PersonalInformation
+                {
+                    Name = "Joe",
+                    DateOfBirth = DateTime.Parse("2001-9-11"),
+                    SocialSecurityNumber = "618432783",
+                    address = "180 Greenwich Street New York, NY 10281 United States"
+                },
+                new PersonalInformation
+                {
+                    Name = "Matt",
+                    DateOfBirth = DateTime.Parse("0001-12-25"),
+                    SocialSecurityNumber = "1",
+                    address = "Bethlehem, Israel"
+                },
+                new PersonalInformation
+                {
+                    Name = "Sam",
+                    DateOfBirth = DateTime.Parse("2024-2-21"),
+                    SocialSecurityNumber = "521736429",
+                    address = "Nuts, Deez"
+                },
+                new PersonalInformation
+                {
+                    Name = "Lebron James",
+                    DateOfBirth = DateTime.Parse("2002-8-3"),
+                    SocialSecurityNumber = "721468348",
+                    address = "Somewhere, United States"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
--- JoesFunny/Program.cs
+++ JoesFunny/Program.cs
@@ -1,16 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using JoesFunny.Data;
+using JoesFunny.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<JoesFunnyContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("JoesFunnyContext") ?? throw new InvalidOperationException("Connection string 'JoesFunnyContext' not found.")));
-
-
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
