3db5910fc6b3ace1785b9f4aba4e9261a2cde6ab
JoeYalden <joeyald@outlook.com>
2024-02-28 18:40:06 +0000

    ^

--- JoesFunny/Tests/PageTests.cs
+++ JoesFunny/Tests/PageTests.cs
@@ -7,18 +7,15 @@ namespace JoesFunny.Tests
 {
     public class PageTests
     {
+        IWebDriver driver = new EdgeDriver();
         [SetUp]
         public void setUp() {
-            IWebDriver driver = new EdgeDriver();
-            //IWebDriver driver = new ChromeDriver("C:\\Users\\Joeya\\.cache\\selenium\\chrome\\win64\\122.0.6261.94\\chrome.exe");
 
             driver.Navigate().GoToUrl("http://localhost:5167"); 
         }
         [Test]
         public void TestPage()
         {
-            IWebDriver driver = new EdgeDriver();
-            driver.Navigate().GoToUrl("http://localhost:5167");
             var nameFilter = driver.FindElement(By.XPath("/ html / body / div / main / form / p / input[1]"));
             nameFilter.Click();
             nameFilter.SendKeys("Lebron James");
