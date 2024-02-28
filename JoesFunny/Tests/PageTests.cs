2288258cd2f366e67fec8fba1b0a04c784f8cc95
JoeYalden <joeyald@outlook.com>
2024-02-28 18:39:39 +0000

    Ohhhh myyyy dayssss
    Fixed the stupid chromedriver issue by just using edge(stupid google)

--- JoesFunny/Tests/PageTests.cs
+++ JoesFunny/Tests/PageTests.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
 using NUnit.Framework;
+using OpenQA.Selenium.Edge;
 
 namespace JoesFunny.Tests
 {
@@ -8,15 +9,16 @@ namespace JoesFunny.Tests
     {
         [SetUp]
         public void setUp() {
-            IWebDriver driver = new ChromeDriver("C:\\Program Files\\Google\\Chrome");
-            
-            driver.Navigate().GoToUrl("https://localhost:5167");
-        } 
+            IWebDriver driver = new EdgeDriver();
+            //IWebDriver driver = new ChromeDriver("C:\\Users\\Joeya\\.cache\\selenium\\chrome\\win64\\122.0.6261.94\\chrome.exe");
+
+            driver.Navigate().GoToUrl("http://localhost:5167"); 
+        }
         [Test]
         public void TestPage()
         {
-            IWebDriver driver = new ChromeDriver();
-            //driver.Navigate().GoToUrl("https://localhost:5167");
+            IWebDriver driver = new EdgeDriver();
+            driver.Navigate().GoToUrl("http://localhost:5167");
             var nameFilter = driver.FindElement(By.XPath("/ html / body / div / main / form / p / input[1]"));
             nameFilter.Click();
             nameFilter.SendKeys("Lebron James");
